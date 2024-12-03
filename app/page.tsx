a93c0b6fcbdde711463cedddcaa466661ee1beb8
Chaz Wilder <cwilder@chazwilder.onmicrosoft.com>
2024-12-03 15:01:35 -0500

    Button Init

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import {Button} from "@/components/ui/button";
 
 const Home = () => {
     return (
-        <div>
-            <p className="text-green-500 font-bold">Hello, Lingo</p>
+        <div className="p-10 flex flex-col items-center justify-center mx-auto gap-8 space-y-8">
+            <Button>Default</Button>
+            <Button>Primary</Button>
+            <Button>Primary</Button>
+            <Button>Primary</Button>
+
         </div>
     )
 }
-export default Home
+export default Home;
