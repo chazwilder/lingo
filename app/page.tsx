68876783cd4d6dfc86cafbe632a6dd3f063b27f1
Chaz Wilder <cwilder@chazwilder.onmicrosoft.com>
2024-12-06 20:52:52 -0500

    Button Library Completed

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,6 @@ const Home = () => {
     return (
         <div className="p-10 flex flex-col items-center justify-center mx-auto gap-8 space-y-8">
             <Button>Default</Button>
-            <Button>Primary</Button>
-            <Button>Primary</Button>
-            <Button>Primary</Button>
-
         </div>
     )
 }
