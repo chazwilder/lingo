15e8a905f4a7aaf8a8365afb9015ae04cea7fd4a
Chaz Wilder <cwilder@chazwilder.onmicrosoft.com>
2024-12-07 21:14:04 -0500

    Marketing Skelly Completed

---
+++ components/layout/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="w-full h-20 border-b border-slate-200 px-4">
+            Header
+        </div>
+    )
+}
+export default Header
